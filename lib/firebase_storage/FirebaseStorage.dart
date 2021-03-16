1ed0ee763b9c6d991621ba8be2092b620acd60b7
Tuba KOCABIYIK <tubakocabiyik@gmail.com>
2021-03-16 21:31:14 +0300

    firebase storage added

--- lib/firebase_storage/FirebaseStorage.dart
+++ lib/firebase_storage/FirebaseStorage.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:regexpattern/regexpattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FStorage extends StatefulWidget {
   // This widget is the root of your application.
@@ -14,7 +18,9 @@ class _StorageState extends State<FStorage> {
   var formKey = GlobalKey<FormState>();
   bool validate = false;
   String mail;
-
+  PickedFile _pickedFile;
+  final imagePicker = ImagePicker();
+  var url;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,31 +68,122 @@ class _StorageState extends State<FStorage> {
           color: Colors.purple.shade200,
           child: Text("save"),
         ),
+        RaisedButton(
+          onPressed: () {
+            if (formKey.currentState.validate()) {
+              formKey.currentState.save();
+              getData();
+            }
+          },
+          color: Colors.greenAccent.shade200,
+          child: Text("getData"),
+        ),
+        RaisedButton(
+          onPressed: () {
+            deleteData();
+          },
+          color: Colors.red.shade300,
+          child: Text("delete"),
+        ),
+        RaisedButton(
+          onPressed: () {
+            gallery();
+          },
+          color: Colors.pinkAccent.shade100,
+          child: Text("take photo from gallery"),
+        ),
+        RaisedButton(
+          onPressed: () {
+            camera();
+          },
+          color: Colors.blue.shade200,
+          child: Text("camera"),
+        ),
+        Expanded(
+          child: Center(
+            child: Container(
+              child: _pickedFile == null
+                  ? Text("")
+                  : Image.file(File(_pickedFile.path)),
+            ),
+          ),
+        )
       ]),
     );
   }
 
   void saveData() {
+
+    firebaseFirestore.collection("users").doc("getID").set({'image':url});
+
+
+
+
+
     String getID = firebaseFirestore.collection("users").doc().id;
-    firebaseFirestore.doc("/users/$getID").set({"data": mail,"UId:": getID});
-    firebaseFirestore.doc("/users/getID").update({'name':"tubişş",'like':FieldValue.increment(1),'para': 500});
+    firebaseFirestore.doc("/users/$getID").set({"data": mail, "UId:": getID});
+    firebaseFirestore
+        .doc("/users/getID")
+        .update({'like': FieldValue.increment(1), 'para': 500});
 
     DocumentReference getRef = firebaseFirestore.doc("/users/getID");
-    
+
     firebaseFirestore.runTransaction((transaction) async {
       DocumentSnapshot getData = await getRef.get();
-    if(getData.exists){
-      if(getData.data()['para']>200){
-        transaction.update(getRef, {'para':getData.data()['para']-200 });
+      if (getData.exists) {
+        if (getData.data()['para'] > 200) {
+          transaction.update(getRef, {'para': getData.data()['para'] - 200});
+        }
+      } else {
+        debugPrint("data cant take");
       }
-
-
-    }else{
-      debugPrint("data cant take");
-    }
-
     });
+  }
 
+  void deleteData() {
+    firebaseFirestore.doc("/users/getID").update({'name': FieldValue.delete()});
+  }
 
+  void getData() async {
+    DocumentSnapshot snapshot =
+        await firebaseFirestore.doc("/users/getID").get();
+    if (snapshot.exists) {
+      String gettingData = snapshot.data()['$mail'];
+    } else {
+      debugPrint("dont take data");
+    }
+    DocumentReference documentReference = firebaseFirestore.doc("/users/getID");
+    documentReference.snapshots().listen((event) {});
+
+    QuerySnapshot querySnapshot = await firebaseFirestore
+        .collection("users")
+        .where("name", isEqualTo: "Beth hormon")
+        .get();
+    for (var docs in querySnapshot.docs) {
+      debugPrint(docs.data().toString());
+    }
+  }
+
+  void gallery() async {
+    var photo = await imagePicker.getImage(source: ImageSource.gallery);
+    setState(() {
+      _pickedFile = photo;
+    });
+    StorageReference ref = FirebaseStorage.instance
+        .ref()
+        .child("Name")
+        .child("user")
+        .child("tu.png");
+    StorageUploadTask storageUploadTask =
+        await ref.putFile(File(_pickedFile.path));
+     url = await (await storageUploadTask.onComplete).ref.getDownloadURL();
+    debugPrint(url);
+  }
+
+  void camera() async {
+    var photo = await imagePicker.getImage(source: ImageSource.camera);
+    setState(() {
+      _pickedFile = photo;
+    });
   }
 }
